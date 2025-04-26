67e52d48e91a81e9c55bb20b768440513f54a932
Stephanie Li <stephanie.mingwei.li@gmail.com>
2025-04-26 18:35:10 -0400

    added to scraper.php so that admin users can add scraped websites to the database

--- php/connect.php
+++ php/connect.php
@@ -2,9 +2,16 @@
 
     try {
         $dbh = new PDO(
-            "mysql:host=localhost;dbname=faghanim_db",
-            "faghanim",
-            "13821382Mo@"
+            // "mysql:host=localhost;dbname=faghanim_db",
+            // "faghanim",
+            // "13821382Mo@"
+            "mysql:host=localhost;dbname=lazy_spoon",
+            "root",
+            ""
+            // "mysql:host=localhost;dbname=li3424_db",
+            // "li3424_local",
+            // "uDqHFzSw"
+
         );
     }
     catch (Exception $e) {
